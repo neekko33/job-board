3fdb1bc2f8b68fbd5836efd9319450aeaa373a23
neekko33 <neekko33@gmail.com>
2025-09-06 15:04:39 +0800

    fix bug

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -6,7 +6,7 @@
     </div>
     @foreach($optionsWithLabels as $label => $option)
         <div class="flex items-center">
-            <input type="radio" name="experience" id="{{$option}}" class="mr-1"
+            <input type="radio" name="{{$name}}" id="{{$option}}" class="mr-1"
                    value="{{$option}}" @checked(request($name) === $option)/>
             <label for="{{$option}}">{{$label}}</label>
         </div>
