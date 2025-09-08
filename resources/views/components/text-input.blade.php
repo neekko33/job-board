a55c69b491ea340405d4c2bfb5281f5d67182281
neekko33 <neekko33@gmail.com>
2025-09-08 22:58:49 +0800

    update error ui

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,22 +1,25 @@
-<div {{$attributes->merge(['class' => 'relative'])}}>
-    <input
-        x-ref="input-{{$name}}"
-        type="{{$type}}"
-        placeholder="{{$placeholder}}"
-        name="{{$name}}"
-        value="{{$value}}"
-        class="w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2 pr-8"
-    />
-    @if($formRef)
-        <button
-            type="button"
-            class="absolute top-0 right-0 h-full w-8 flex items-center justify-center hover:text-slate-500 text-slate-400 cursor-pointer"
-            @click="$refs['input-{{$name}}'].value = '';$refs['{{$formRef}}'].submit()">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                 class="size-4">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/>
-            </svg>
-        </button>
-    @endif
+<div {{ $attributes }}>
+    <div class="relative">
+        <input x-ref="input-{{ $name }}" type="{{ $type }}" placeholder="{{ $placeholder }}"
+            name="{{ $name }}" value="{{ old($name, $value)}}" @class([
+                'w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2 ring-slate-300',
+                'pr-8' => $formRef,
+                '!ring-red-500' => $errors->has($name),
+            ]) />
+        @if ($formRef)
+            <button type="button"
+                class="absolute top-0 right-0 h-full w-8 flex items-center justify-center hover:text-slate-500 text-slate-400 cursor-pointer"
+                @click="$refs['input-{{ $name }}'].value = '';$refs['{{ $formRef }}'].submit()">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+            </button>
+        @endif
+    </div>
+    <div>
+        @error($name)
+            <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
+        @enderror
+    </div>
 </div>
-
